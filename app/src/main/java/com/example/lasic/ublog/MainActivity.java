6b3967d2ae8d51ba78771095b54a37163fb33255
Josip Lasic <lasic.josip96@gmail.com>
2017-10-16 19:34:28 +0200

    response test

--- app/src/main/java/com/example/lasic/ublog/MainActivity.java
+++ app/src/main/java/com/example/lasic/ublog/MainActivity.java
@@ -2,6 +2,19 @@ package com.example.lasic.ublog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.example.lasic.ublog.singletons.RequestManager;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JsonArrayRequest jsObjRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                "https://jsonplaceholder.typicode.com" + "/posts",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("TEST123a", "onResponse: \n" + response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("TEST123a", "onErrorResponse: " + error.getMessage());
+                    }
+                });
+        RequestManager.getInstance(this).addToRequestQueue(jsObjRequest);
     }
 }
